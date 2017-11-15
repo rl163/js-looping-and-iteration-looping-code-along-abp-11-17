c6844a0f4b4928f14cadcf17e0496ca63c7a8100
Ryan Lee <ryanlee91@gmail.com>
2017-11-15 02:52:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function printBadges(employee) {
   for (let i=0; i < employee.length; i++){
   console.log(`Welcome ${employee[i]}! You are employee #${i}`);
 }
-return employee
+return employee;
 }

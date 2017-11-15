74204a40f0e93b333db07e4fe7d0646ed1f09c54
Ryan Lee <ryanlee91@gmail.com>
2017-11-15 02:49:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+employee = ['Ada','Brendan','Ali']
+
+function printBadges(employee) {
+  for (let i=o; i < employee.length; i++){}
+  console.log(Welcome ${employee[i]}! You are employee #${i})
+}

3eb84b3b0e3ee8d5a0fb6fce204813c7423390b7
Ryan Lee <ryanlee91@gmail.com>
2017-11-15 02:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,10 @@ printBadges(employee);
 
 
 function tailsNeverFails(){
-  
+  let tails = 0;
+  while (Math.random() >= 0.5){
+    tails++;
+  }
+
+  return `You\'ve got $(tails} tails in a row!)`
 }
